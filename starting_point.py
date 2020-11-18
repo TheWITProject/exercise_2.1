f88b8a89c237fda043e42535c2d9375327b74371
Olivia Sztanga <oliviasztanga@gmail.com>
2020-11-18 00:25:01 -0500

    Create starting_point.py

---
+++ starting_point.py
@@ -0,0 +1,24 @@
+class Deck:
+  
+  def __init__(self):
+    # define instance attributes here
+
+  def generate_cards(self):
+    # define this method here
+    # also, where should this method be called?
+  
+  def reset_cards(self):
+    # define this method here
+  
+  def shuffle_cards(self):
+    # define this method here
+
+
+class Card:
+
+  def __init__(self):
+    # define instance attributes here
+  
+  def flip_card(self):
+    # define this method here
+  
