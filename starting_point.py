4fd179c794ba5b84866a76af27e35049944431d7
suhaimaislam <73371083+suhaimaislam@users.noreply.github.com>
2020-11-23 20:18:54 -0500

    Update starting_point.py

--- starting_point.py
+++ starting_point.py
@@ -1,24 +1,32 @@
+import random
+
 class Deck:
-  
-  def __init__(self):
-    # define instance attributes here
+    def __init__(self,cards,generate_cards):
+        self.cards = []
+        self.generate_cards()
 
-  def generate_cards(self):
-    # define this method here
-    # also, where should this method be called?
-  
-  def reset_cards(self):
-    # define this method here
-  
-  def shuffle_cards(self):
-    # define this method here
+    def generate_cards(self):
+        for s in ["Spades", "Diamonds", "Hearts", "Clubs"]:
+            for v in range(1,14):
+                self.cards.append(Card(s, v))
+                
+    def flip(self):
+        for c in self.cards:
+            c.flip()
 
+    def shuffle_cards(self):
+        for i in range(len(self.cards)):
+            r = random.randint(0, i)
+            self.cards[i], self.cards[r] = self.cards[r], self.cards[i]
+
+    def draw(self):
+        return self.cards.pop()
 
 class Card:
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
 
-  def __init__(self):
-    # define instance attributes here
-  
-  def flip_card(self):
-    # define this method here
-  
+    def flip_card(self):
+        print(f"{self.rank} of {self.suit}")
+
