449076ded9ff52a582bac65565eca6521f3ee283
jungaha23 <54672053+jungaha23@users.noreply.github.com>
2020-11-24 17:54:10 -0800

    Update starting_point.py

--- starting_point.py
+++ starting_point.py
@@ -1,7 +1,9 @@
 class Deck:
   
-  def __init__(self):
+  def __init__(self, type_of_deck):
+    number_of_cards = 52
     # define instance attributes here
+    self.type_of_deck = type_of_deck    
 
   def generate_cards(self):
     # define this method here
@@ -16,9 +18,13 @@ class Deck:
 
 class Card:
 
-  def __init__(self):
+  def __init__(self, suit, value, orientation):
     # define instance attributes here
+    self.suit = suit
+    self.value = value
+    self.orientation = orientation #orientation means facing up or down
   
   def flip_card(self):
     # define this method here
+    
   
